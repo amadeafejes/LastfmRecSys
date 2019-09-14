c5845d04e85a4f19e3e4ad2969096a4f4c04f3a0
Amadea Fejes <amadea.fejes@gmail.com>
2019-09-14 13:52:06 +0200

    fix: handle columns with commas in P02
    use a different approach in registered column from user file

--- Project 02 - Original Project on GCP/Spark job files/01_export_users.scala
+++ Project 02 - Original Project on GCP/Spark job files/01_export_users.scala
@@ -7,13 +7,14 @@ val header = rawData.first()
 val filteredData = rawData.filter(_(0) != header(0))
 filteredData.first
 
-case class User(id:String,gender:String,age:Int,country:String,registered:String)
-val userObjects = filteredData.map(_.split("\t")).map(s => User(s(0),s(1),s(2),s(3),s(4))
-
 def convertUserId(tag: String):Int = {
 	return tag.substring(5).toInt
 }
 
-val users = userObjects.map(s => (convertUserId(s(0))+ "," + s(0) + "," + s(1) + "," + s(2) + "," + s(3) + "," + '"' + s(4) + '"'))
+val users = filteredData.map(s => (convertUserId(s.substring(0, 11)) + '\t' + s))
+users.first
+
+val editedUsers = users.map(s => s.replace(',', '.')).map(s => s.replace('\t', ','))
+editedUsers.first
 
 users.saveAsTextFile(file)
